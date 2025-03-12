d135c78a514eee5d421776aae98998ce4c030424
unknown <aphysics.nerd@gmail.com>
2025-03-12 10:23:05 +0330

    .

--- api/index.py
+++ api/index.py
@@ -2,10 +2,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+users = {
+    "astro": "astro1388",
+    "bemami": "bioc"
+}
+
 @app.route("/")
 def home():
     return "\nWELCOME TO PROMETHEUS\n"
 
 @app.route('/gate', metods=["POST"])
 def authenticator():
-    pass
+    id = request.args.get("id")
+    passkey = request.args.get("passkey")
+
+    if id in users.keys and passkey == users[id]:
+        return "USER FOUND"
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from requests import get, post
 
 
-response = post("https://promethe.vercel.app/")
+response = post("https://promethe.vercel.app?id=astro&passkey=astro1388")
 
 print(response.text)
